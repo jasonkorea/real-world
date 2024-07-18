fa3bd0c1bd7baccfa32de483dd92eab7b43d4b00
jasonkorea <jaehoon85@gmail.com>
2024-07-18 22:21:45 +0900

    chatgpt용 압축 쉘 스크립트 작성

---
+++ chatgpt_pack_powershell.ps1
@@ -0,0 +1,1 @@
+Compress-Archive -Path db,middleware,models,public,routes,app.js,jsconfig.json,nodemon.json,package.json,package-lock.json -DestinationPath google_map_game.zip
--- pack.bat
+++
@@ -1,1 +0,0 @@
-7z a google_map_game.7z db middleware models public routes app.js jsconfig.json nodemon.json package.json package-lock.json
